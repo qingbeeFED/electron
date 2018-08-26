7bff4225f6bee6a3e74b883934a0fb064c9aa237
Milan Burda <milan.burda@gmail.com>
2018-08-26 10:38:15 +0200

    Add webPreferences.enableRemoteModule option

--- atom/browser/web_contents_preferences.cc
+++ atom/browser/web_contents_preferences.cc
@@ -181,6 +181,10 @@ bool WebContentsPreferences::GetPreference(const base::StringPiece& name,
   return GetAsString(&preference_, name, value);
 }
 
+bool WebContentsPreferences::IsRemoteModuleEnabled() const {
+  return IsEnabled(options::kEnableRemoteModule, true);
+}
+
 bool WebContentsPreferences::GetPreloadPath(
     base::FilePath::StringType* path) const {
   DCHECK(path);
@@ -276,6 +280,10 @@ void WebContentsPreferences::AppendCommandLineSwitches(
         command_line->AppendArg(customArg.GetString());
     }
   }
+
+  // Whether to enable the remote module
+  if (!IsRemoteModuleEnabled())
+    command_line->AppendSwitch(switches::kDisableRemoteModule);
 
   // Run Electron APIs and preload script in isolated world
   if (IsEnabled(options::kContextIsolation))
